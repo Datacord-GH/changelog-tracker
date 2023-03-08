d3dd7d1f1dd94eea70aa409007c4558a4bdec0a6
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2023-03-08 20:11:35 +0100

    actually handled videos lol

--- src/discord.rs
+++ src/discord.rs
@@ -1,3 +1,4 @@
+use serenity::builder::CreateEmbed;
 use serenity::prelude::SerenityError;
 use serenity::{http::Http, model::channel::Embed, model::webhook::Webhook, utils::Colour};
 use std::env;
@@ -10,14 +11,23 @@ pub async fn send_message(changelog: &Changelog, client: &Client) -> Result<(), 
     let webhook = Webhook::from_url(&http, &token).await?;
     let fail_safe_asset = String::from("https://cdn.discordapp.com/embed/avatars/0.png");
 
+    let asset = match changelog.asset_type {
+        0 => "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/800px-Logo_of_YouTube_%282015-2017%29.svg.png",
+        1 => &changelog.asset.as_ref().unwrap_or_else(|| &fail_safe_asset),
+        _ => &fail_safe_asset,
+    };
+
     let changelog_embed = Embed::fake(|e| {
         e.colour(Colour::from_rgb(135, 134, 255))
             .field("Changelog Id", &changelog.changelog_id, true)
             .field("Entry Id", &changelog.entry_id, true)
             .field("Asset Type", changelog.asset_type, true)
-            .description(&changelog.content)
+            .description(format!(
+                "{}\n\n**__Asset Link__**\n{}",
+                &changelog.content, &asset
+            ))
             .footer(|f| f.text(format!("{} • {}", changelog.date, changelog.locale)))
-            .image(&changelog.asset.as_ref().unwrap_or_else(|| &fail_safe_asset))
+            .image(&asset)
             .title(format!("Changelog on {:#?}", client))
     });
 
--- src/main.rs
+++ src/main.rs
@@ -35,54 +35,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let config_body = reqwest::get(&config_url).await?.text().await?;
         let config: HashMap<String, ChangelogConfig> = serde_json::from_str(&config_body)?;
 
-        let mut snowflake: String = String::new();
-        let mut min_version: usize = 0;
+        for (snowflake, value) in config {
+            let min_version = value.min_version;
 
-        for (key, value) in config {
-            snowflake = key;
-            min_version = value.min_version;
-        }
+            let sql_select = format!(
+                "SELECT * FROM changelogs WHERE changelog_id = '{}' AND client = '{}'",
+                snowflake, client_id
+            );
+            let mut stmt = conn.prepare(&sql_select)?;
+            let changelog_db = stmt.query_map([], |row| {
+                Ok(ChangelogDB {
+                    changelog_id: row.get(0)?,
+                    client: row.get(1)?,
+                    locale: row.get(2)?,
+                    date: row.get(3)?,
+                    asset: row.get(4)?,
+                    asset_type: row.get(5)?,
+                    content: row.get(6)?,
+                })
+            })?;
 
-        let sql_select = format!(
-            "SELECT * FROM changelogs WHERE changelog_id = '{}' AND client = '{}'",
-            snowflake, client_id
-        );
-        let mut stmt = conn.prepare(&sql_select)?;
-        let changelog_db = stmt.query_map([], |row| {
-            Ok(ChangelogDB {
-                changelog_id: row.get(0)?,
-                client: row.get(1)?,
-                locale: row.get(2)?,
-                date: row.get(3)?,
-                asset: row.get(4)?,
-                asset_type: row.get(5)?,
-                content: row.get(6)?,
-            })
-        })?;
+            if changelog_db.count() > 0 {
+                println!("[{:#?}] Still on {}", client, snowflake);
+                continue;
+            }
 
-        if changelog_db.count() > 0 {
-            println!("[{:#?}] Still on {}", client, snowflake);
-            continue;
-        }
+            println!(
+                "[{:#?}] Fetching the changelog for {} with min version of {}",
+                client, snowflake, min_version
+            );
 
-        println!(
-            "[{:#?}] Fetching the changelog for {} with min version of {}",
-            client, snowflake, min_version
-        );
+            let changelog_url = format!(
+                "https://cdn.discordapp.com/changelogs/{}/{}/en-US.json",
+                client_id, snowflake
+            );
+            let changelog = reqwest::get(&changelog_url)
+                .await?
+                .json::<Changelog>()
+                .await?;
 
-        let changelog_url = format!(
-            "https://cdn.discordapp.com/changelogs/{}/{}/en-US.json",
-            client_id, snowflake
-        );
-        let changelog = reqwest::get(&changelog_url)
-            .await?
-            .json::<Changelog>()
-            .await?;
+            println!("[{:#?}] Found changelog: {}", client, changelog.date);
+            send_message(&changelog, &client).await?;
 
-        println!("[{:#?}] Found changelog: {}", client, changelog.date);
-        send_message(&changelog, &client).await?;
-
-        conn.execute(
+            conn.execute(
             "INSERT INTO changelogs (changelog_id, client, locale, date, asset, asset_type, content) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
             (
                &changelog.changelog_id,
@@ -94,6 +89,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                &changelog.content,
             ),
         )?;
+        }
     }
 
     Ok(())
